7570a3a46d6b1c9bf22d20e8ca1088ea008d39de
PawarAniket <Aniket.PAWAR@Cassiopae.com>
2018-05-14 19:03:39 +0530

    Created default route (PageNotFound) for URL "/***" i.e any route follwoed by "/".

---
+++ src/NotFound/components/NotFoundContainer.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class NotFoundContainer extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>
+                    Page not found....
+                </p>
+            </div>
+        );
+    }
+}
+
+export default NotFoundContainer;
--- src/Route/components/Routing.jsx
+++ src/Route/components/Routing.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import HomeContainer from '../../Home/components/HomeContainer';
-//import NotFound from '../NotFound/NotFound';
+import NotFoundContainer from '../../NotFound/components/NotFoundContainer';
 import '../styles/route.css';
 
 const Routing = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={HomeContainer}/>
+            <Route path="*" component={NotFoundContainer}/>
         </Switch>
     </Router>
 );
-
-//         <Route path="*" component={NotFound} />
 
 export default Routing;
